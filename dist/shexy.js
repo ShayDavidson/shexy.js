3a9087689c89e6da63c63ea3886d0307c8667d2b
Shay Davidson <shay.h.davidson@gmail.com>
2016-01-20 23:23:46 +0200

    Removed babel lib compilation for now

--- dist/shexy.js
+++ dist/shexy.js
@@ -48,9 +48,8 @@ var Shexy =
 	'use strict';
 	
 	Object.defineProperty(exports, "__esModule", {
-	  value: true
+	    value: true
 	});
-	exports.Vector = exports.ObjectUtils = exports.Iterators = exports.Direction = exports.Hex = exports.Board = exports.Designer = undefined;
 	
 	var _designer = __webpack_require__(1);
 	
@@ -60,27 +59,29 @@ var Shexy =
 	
 	var _direction = __webpack_require__(7);
 	
-	var _Direction = _interopRequireWildcard(_direction);
+	var Direction = _interopRequireWildcard(_direction);
 	
 	var _iterators = __webpack_require__(3);
 	
-	var _Iterators = _interopRequireWildcard(_iterators);
+	var Iterators = _interopRequireWildcard(_iterators);
 	
 	var _object = __webpack_require__(4);
 	
-	var _ObjectUtils = _interopRequireWildcard(_object);
+	var ObjectUtils = _interopRequireWildcard(_object);
 	
 	var _vector = __webpack_require__(2);
 	
 	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 	
-	var Designer = exports.Designer = _designer._Designer;
-	var Board = exports.Board = _board._Board;
-	var Hex = exports.Hex = _hex._Hex;
-	var Direction = exports.Direction = _Direction;
-	var Iterators = exports.Iterators = _Iterators;
-	var ObjectUtils = exports.ObjectUtils = _ObjectUtils;
-	var Vector = exports.Vector = _vector._Vector;
+	exports.default = {
+	    Designer: _designer.Designer,
+	    Board: _board.Board,
+	    Hex: _hex.Hex,
+	    Direction: Direction,
+	    Iterators: Iterators,
+	    ObjectUtils: ObjectUtils,
+	    Vector: _vector.Vector
+	};
 
 /***/ },
 /* 1 */
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import { _Designer } from 'curves/designer'
-import { _Board } from 'models/board'
-import { _Hex } from 'models/hex'
-import * as _Direction from 'utils/direction'
-import * as _Iterators from 'utils/iterators'
-import * as _ObjectUtils from 'utils/object'
-import { _Vector } from 'utils/vector'
+import { Designer } from 'curves/designer'
+import { Board } from 'models/board'
+import { Hex } from 'models/hex'
+import * as Direction from 'utils/direction'
+import * as Iterators from 'utils/iterators'
+import * as ObjectUtils from 'utils/object'
+import { Vector } from 'utils/vector'
 
-export let Designer = _Designer
-export let Board = _Board
-export let Hex = _Hex
-export let Direction = _Direction
-export let Iterators = _Iterators
-export let ObjectUtils = _ObjectUtils
-export let Vector = _Vector
+export default {
+    Designer: Designer,
+    Board: Board,
+    Hex: Hex,
+    Direction: Direction,
+    Iterators: Iterators,
+    ObjectUtils: ObjectUtils,
+    Vector: Vector
+}
