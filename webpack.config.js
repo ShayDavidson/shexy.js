d86bdbc00b0253bb911f1dd445d0135b64510001
Shay Davidson <sdavidson@paypal.com>
2016-11-03 09:17:27 +0200

    Migrated to yarn

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-var webpack = require('webpack')
+const webpack = require('webpack')
 
 module.exports = {
-    entry: {
-        'shexy': './src/index.js',
-        'shexy.min': './src/index.js'
-    },
-    output: {
-        libraryTarget: 'var',
-        library: 'Shexy',
-        path: __dirname + '/dist',
-        filename: '[name].js'
-    },
-    optimize: {
-        UglifyJsPlugin: true
-    },
-    module: {
-        loaders: [{
-            test: /\.js?$/,
-            exclude: /(node_modules)/,
-            loader: 'babel'
-        }]
-    },
-    plugins: [
-        new webpack.NoErrorsPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            include: /\.min\.js$/,
-            minimize: true,
-            compress: {
-                warnings: false
-            }
-        })
-    ],
-    stats: {
-        colors: true
-    },
-    resolve: {
-        modulesDirectories: ['.', 'src', 'node_modules'],
-        extensions: ['', '.js']
-    },
-    devtool: 'source-map'
+	entry: {
+		'shexy': './src/index.js',
+		'shexy.min': './src/index.js'
+	},
+	output: {
+		libraryTarget: 'var',
+		library: 'Shexy',
+		path: __dirname + '/dist',
+		filename: '[name].js'
+	},
+	optimize: {
+		UglifyJsPlugin: true
+	},
+	module: {
+		loaders: [{
+			test: /\.js?$/,
+			exclude: /(node_modules)/,
+			loader: 'babel'
+		}]
+	},
+	plugins: [
+		new webpack.NoErrorsPlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			include: /\.min\.js$/,
+			minimize: true,
+			compress: {
+				warnings: false
+			}
+		})
+	],
+	stats: {
+		colors: true
+	},
+	resolve: {
+		modulesDirectories: ['.', 'src', 'node_modules'],
+		extensions: ['', '.js']
+	},
+	devtool: 'source-map'
 }
