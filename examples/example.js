86f6c9839b47202f9084bd0220cd0e97885d3279
Shay Davidson <sdavidson@paypal.com>
2016-11-26 23:42:09 +0200

    New represenation

--- examples/example.js
+++ examples/example.js
@@ -1,12 +1,22 @@
-var ctx = document.getElementById('canvas').getContext('2d');
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
 
 var size = 20
 var range = 5
 var padding = 0
+var camera = {
+	center: Shexy.View.Point(canvas.width / 2, canvas.height / 2),
+	zoom: 1
+}
 
-var grid = Shexy.Grid.Grid(3)
+var grid = Shexy.Grid.Grid(range)
 Shexy.Grid.gridForEach(grid, function(hex) {
-	console.log(hex.coords.cube)
+	const center = Shexy.View.axialToPoint(hex.coords.axial, size, padding)
+	let corners = Shexy.View.hexCorners(center, size)
+	corners = corners.map((corner) => {
+		return Shexy.View.addPoints(corner, camera.center)
+	})
+	Shexy.Canvas.drawPolygon(ctx, corners, {fillStyle: 'red'})
 })
 
 function draw() {
