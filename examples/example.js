5bba32a9a0dd73d1bcf8ac0fc2622104f4f796ea
Shay Davidson <sdavidson@paypal.com>
2016-11-27 17:34:47 +0200

    Draw vertices

--- examples/example.js
+++ examples/example.js
@@ -1,43 +1,56 @@
-var canvas = document.getElementById('canvas')
-var ctx = canvas.getContext('2d')
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
 
-var size = 20
-var range = 4
-var padding = 10
-var camera = {
+const size = 20
+const range = 4
+const padding = 10
+const camera = {
 	center: Shexy.View.Point(canvas.width / 2, canvas.height / 2),
 	zoom: 1
 }
-var currentHex
-
-var grid = Shexy.Grid.Grid(range)
+let currentHex = null
+const grid = Shexy.Grid.Grid(range)
 
 function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
 	if (currentHex) {
 		document.body.style.cursor = 'pointer'
 	} else {
 		document.body.style.cursor = 'auto'
 	}
-	Shexy.Grid.gridForEach(grid, function(hex) {
-		var center = Shexy.View.axialToPoint(hex.coords.axial, size, padding)
-		var corners = Shexy.View.hexCorners(center, size)
+	Shexy.Grid.gridForEachHex(grid, (hex) => {
+		const center = Shexy.View.axialToPoint(hex.coords.axial, size, padding)
+		let corners = Shexy.View.hexCorners(center, size)
 		corners = corners.map((corner) => {
 			return Shexy.View.addPoints(corner, camera.center)
 		})
-		var color = currentHex && Shexy.Coords.areAxialsEqual(currentHex.coords.axial, hex.coords.axial) ? 'red' : 'gray'
+		let color
+		if (currentHex && Shexy.Coords.areAxialsEqual(currentHex.coords.axial, hex.coords.axial)) {
+			color = 'red'
+			const vertices = Shexy.Coords.cubeVertices(hex.coords.cube)
+			vertices.forEach((vertex) => {
+				let point = Shexy.View.vertexToPoint(vertex, size, padding)
+				point = Shexy.View.addPoints(point, camera.center)
+				ctx.beginPath()
+				ctx.fillStyle = 'blue'
+				ctx.arc(point.x, point.y, padding / 2, 0, 2 * Math.PI, false)
+				ctx.stroke()
+			})
+		} else {
+			color = 'gray'
+		}
 		Shexy.Canvas.drawPolygon(ctx, corners, {fillStyle: color})
 	})
 }
 
 function getMousePos(canvas, event) {
-	var rect = canvas.getBoundingClientRect()
+	const rect = canvas.getBoundingClientRect()
 	return { x: event.clientX - rect.left, y: event.clientY - rect.top }
-};
+}
 
-canvas.addEventListener('mousemove', function(event) {
-	var pos = Shexy.View.subtractPoints(getMousePos(canvas, event), camera.center)
-	var currentAxial = Shexy.View.pointToAxial(pos, size, padding)
+canvas.addEventListener('mousemove', (event) => {
+	const pos = Shexy.View.subtractPoints(getMousePos(canvas, event), camera.center)
+	const currentAxial = Shexy.View.pointToAxial(pos, size, padding)
 	currentHex = Shexy.Grid.hexAt(grid, currentAxial)
 	draw()
 })
